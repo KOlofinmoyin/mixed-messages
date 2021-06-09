585b8b4b931d6f47535ac49b6ee5486ad33b903f
Kehinde Olofinmoyin <kehindeolofinmoyin@hotmail.com>
2021-06-09 17:06:05 +0100

    Store message components

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 
 doodle = {
+    _doodle:{
+        who:[], 
+        what:[],
+        where:[]
+        },
     respond: ()=> {
         console.log("We're cooking on gas!")
     }
 }
 
+//Tests:
 doodle.respond()
+console.log(doodle._doodle.who)
